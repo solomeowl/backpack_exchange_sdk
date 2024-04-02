e3842a2498a3ad787fe8610f15460f15444bca45
solomeowl <j19940430@gmail.com>
2024-04-03 00:14:11 +0800

    fix: get user balance invalid signature issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="backpack_exchange_sdk",
-    version="0.3.0",
+    version="0.3.1",
     author="solomeowl",
     author_email="j19940430@gmail.com",
     description="A simple SDK for backpack exchange",
