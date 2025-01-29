11cb8de86fb0813791f962b2bf14d61c28fac68d
solomeowl <steve.cheng0430@gmail.com>
2025-01-30 02:32:55 +0800

    style: format code with black and fix flake8 issues
    - Add code formatting tools (black, isort, flake8)
    - Fix line length issues
    - Clean up imports
    - Add development requirements

--- examples/example_public.py
+++ examples/example_public.py
@@ -1,4 +1,5 @@
 import time
+
 from backpack_exchange_sdk.public import PublicClient
 
 public_client = PublicClient()
@@ -16,28 +17,28 @@ print(public_client.get_collateral())
 print(public_client.get_markets())
 
 # Get specific market
-print(public_client.get_market('SOL_USDC'))
+print(public_client.get_market("SOL_USDC"))
 
 # Get ticker for a specific market
-print(public_client.get_ticker('SOL_USDC'))
+print(public_client.get_ticker("SOL_USDC"))
 
 # Get tickers for all markets
 print(public_client.get_tickers())
 
 # Get order book depth for a specific market
-print(public_client.get_depth('SOL_USDC'))
+print(public_client.get_depth("SOL_USDC"))
 
 # Get K-Lines for a specific market
-print(public_client.get_klines('SOL_USDC', '1m', int(time.time())))
+print(public_client.get_klines("SOL_USDC", "1m", int(time.time())))
 
 # Get mark price for a specific market
-print(public_client.get_mark_price('SOL_USDC_PERP'))
+print(public_client.get_mark_price("SOL_USDC_PERP"))
 
 # Get open interest for a specific market
-print(public_client.get_open_interest('SOL_USDC_PERP'))
+print(public_client.get_open_interest("SOL_USDC_PERP"))
 
 # Get funding interval rates for a specific market
-print(public_client.get_funding_interval_rates('SOL_USDC_PERP'))
+print(public_client.get_funding_interval_rates("SOL_USDC_PERP"))
 
 # ================================================================
 # System - Exchange system status.
@@ -55,7 +56,7 @@ print(public_client.get_system_time())
 # Trades - Public trade data.
 # ================================================================
 # Get recent trades for a specific market
-print(public_client.get_recent_trades('SOL_USDC_PERP'))
+print(public_client.get_recent_trades("SOL_USDC_PERP"))
 
 # Get historical trades for a specific market
-print(public_client.get_historical_trades('SOL_USDC_PERP', 10, 0))
+print(public_client.get_historical_trades("SOL_USDC_PERP", 10, 0))
