774206c6ce042e56ce8f2b71ced18cd272f2ee60
solomeowl <86129240+solomeowl@users.noreply.github.com>
2025-01-30 01:56:31 +0800

    Revert "release/1.0.25"

--- backpack_exchange_sdk/public.py
+++ backpack_exchange_sdk/public.py
@@ -19,7 +19,8 @@ class PublicClient:
         else:
             try:
                 error = response.json()
-                raise Exception(f"API Error: {error.get('code')} - {error.get('message')}")
+                raise Exception(f"API Error: {error.get(
+                    'code')} - {error.get('message')}")
             except ValueError:
                 raise Exception(f"HTTP Error {response.status_code}: {response.text}")
 
