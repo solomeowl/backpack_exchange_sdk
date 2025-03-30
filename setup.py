54e993605c82c9f02e0afe1fe7a986beaac0b407
solomeowl <j19940430@gmail.com>
2025-03-31 03:24:45 +0800

    v1.0.34

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="backpack_exchange_sdk",
-    version="1.0.33",
+    version="1.0.34",
     author="solomeowl",
     author_email="j19940430@gmail.com",
     description="A simple SDK for backpack exchange",
